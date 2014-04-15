69bc37d8b665b1d816b915d3b46cd9d293cf98f5
kevivmatrix <vivekpatna52@gmail.com>
2014-04-15 14:10:26 +0530

    fixed solr ping url

--- lib/sunspot_test.rb
+++ lib/sunspot_test.rb
@@ -67,7 +67,7 @@ module SunspotTest
 
     def solr_running?
       begin
-        solr_ping_uri = URI.parse("#{Sunspot.session.config.solr.url}/ping")
+        solr_ping_uri = URI.parse("#{Sunspot.session.original_session.config.solr.url}/ping")
         Net::HTTP.get(solr_ping_uri)
         true # Solr Running
       rescue
